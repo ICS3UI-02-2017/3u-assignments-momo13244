1d031ac9cf5ac83547c69558a9972050f57ca7f2
elmam0030 <elmam0030@HRH-IC0027338.wrdsb.ca>
2018-02-15 11:18:32 -0500

    big boy 123

---
+++ src/assignment2/A1Q1.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2;
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.RobotSE;
+import becker.robots.Thing;
+
+/**
+ *
+ * @author elmam0030
+ */
+public class A1Q1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        // create city
+        City MajorStank = new City();
+
+        // creat robot
+        RobotSE ColonelStank = new RobotSE(MajorStank, 1, 1, Direction.EAST);
+
+        // creat things
+        new Thing(MajorStank, 1, 2);
+        new Thing(MajorStank, 1, 3);
+        new Thing(MajorStank, 1, 4);
+        new Thing(MajorStank, 1, 5);
+        new Thing(MajorStank, 1, 6);
+        new Thing(MajorStank, 1, 7);
+        new Thing(MajorStank, 1, 8);
+        new Thing(MajorStank, 1, 9);
+        new Thing(MajorStank, 1, 10);
+        new Thing(MajorStank, 1, 11);
+
+        while (ColonelStank.getAvenue() != 12) {
+            ColonelStank.move();
+            if (ColonelStank.canPickThing()) {
+                ColonelStank.pickThing();
+            if (ColonelStank.countThingsInBackpack() == 7)
+                ColonelStank.pickThing();
+
+            }
+
+
+            }
+        }
+    }
+}
