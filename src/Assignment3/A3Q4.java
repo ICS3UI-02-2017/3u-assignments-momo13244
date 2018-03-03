bcc4ab5c110b47e4c5c62efecb44a81199c8e84e
Mohamad <Mohamad@192.168.0.22>
2018-03-03 10:41:22 -0500

    big boy 123

--- src/Assignment3/A3Q4.java
+++ src/Assignment3/A3Q4.java
@@ -5,6 +5,11 @@
  */
 package Assignment3;
 
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.RobotSE;
+import becker.robots.Wall;
+
 /**
  *
  * @author Mohamad
@@ -15,7 +20,68 @@ public class A3Q4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        // create the city
+        City woah4 = new City();
+        // create the robot
+        RobotSE momo = new RobotSE(woah4, 3, 3, Direction.SOUTH);
+        
+        // create the squares
+        new Wall(woah4, 1, 1, Direction.WEST);
+        new Wall(woah4, 2, 1, Direction.WEST);
+        new Wall(woah4, 1, 2, Direction.NORTH);
+        new Wall(woah4, 2, 2, Direction.EAST);
+        new Wall(woah4, 1, 1, Direction.NORTH);
+        new Wall(woah4, 2, 2, Direction.SOUTH);
+        new Wall(woah4, 1, 2, Direction.EAST);
+        new Wall(woah4, 2, 2, Direction.SOUTH);
+        new Wall(woah4, 2, 1, Direction.SOUTH);
+        
+        new Wall(woah4, 1, 4, Direction.WEST);
+        new Wall(woah4, 2, 4, Direction.WEST);
+        new Wall(woah4, 1, 5, Direction.NORTH);
+        new Wall(woah4, 2, 5, Direction.EAST);
+        new Wall(woah4, 1, 4, Direction.NORTH);
+        new Wall(woah4, 2, 5, Direction.SOUTH);
+        new Wall(woah4, 1, 5, Direction.EAST);
+        new Wall(woah4, 2, 5, Direction.SOUTH);
+        new Wall(woah4, 2, 4, Direction.SOUTH);
+        
+        new Wall(woah4, 4, 4, Direction.WEST);
+        new Wall(woah4, 5, 4, Direction.WEST);
+        new Wall(woah4, 4, 5, Direction.NORTH);
+        new Wall(woah4, 5, 5, Direction.EAST);
+        new Wall(woah4, 4, 4, Direction.NORTH);
+        new Wall(woah4, 5, 5, Direction.SOUTH);
+        new Wall(woah4, 4, 5, Direction.EAST);
+        new Wall(woah4, 5, 5, Direction.SOUTH);
+        new Wall(woah4, 5, 4, Direction.SOUTH);
+        
+        new Wall(woah4, 4, 1, Direction.WEST);
+        new Wall(woah4, 5, 1, Direction.WEST);
+        new Wall(woah4, 4, 2, Direction.NORTH);
+        new Wall(woah4, 5, 2, Direction.EAST);
+        new Wall(woah4, 4, 1, Direction.NORTH);
+        new Wall(woah4, 5, 2, Direction.SOUTH);
+        new Wall(woah4, 4, 2, Direction.EAST);
+        new Wall(woah4, 5, 2, Direction.SOUTH);
+        new Wall(woah4, 5, 1, Direction.SOUTH);
+        
+        // make him move
+        int numberOfRotations = 4;
+        while (numberOfRotations > 0){            
+            momo.move(3);
+            momo.turnLeft();
+            momo.move(3);
+            momo.turnLeft();
+            momo.move(3);
+            momo.turnLeft();
+            momo.move(3);
+            numberOfRotations = numberOfRotations - 1;
+            
+        }
     }
     
+    
 }
+
