a67e78cb7e1febf3c8cca30d1c9774112ca6459f
Mohamad <Mohamad@192.168.0.22>
2018-03-03 14:02:20 -0500

    big boy 123

---
+++ src/Assignment4/A4Q1.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment4;
+
+import java.util.Scanner;
+import static jdk.nashorn.tools.ShellFunctions.input;
+
+
+/**
+ *
+ * @author Mohamad
+ */
+public class A4Q1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Scan
+        Scanner input = new Scanner(System.in);
+        // ask for their name
+        System.out.println("Hey, what is your name?");
+        // read their name
+        String name = input.nextLine();
+        // greet
+        System.out.println("hello " + name + " how was your day?");
+        
+    }
+    
+}
---
+++ src/Assignment4/A4Q2.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Mohamad
+ */
+public class A4Q2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // scan
+        Scanner input = new Scanner(System.in);
+        // ask for measurement
+        System.out.println("Please enter the measurement in inches you wish to convert");
+        // read measurement
+        int inches = input.nextInt();
+        
+        
+    }
+    
+}
