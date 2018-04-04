75559064396d984e9643e95270db158d78ec99e7
Mohamad <Mohamad@192.168.0.22>
2018-04-03 23:07:08 -0400

    big boy 123

--- src/Assignment5/A5.java
+++ src/Assignment5/A5.java
@@ -22,7 +22,7 @@ public class A5 {
             // ask for the word and store it
             System.out.println("Enter the word you would like to translate");
             String origWord = input.nextLine();
-            // sanatize the input\
+            // sanatize the input
             origWord = origWord.toLowerCase();
             
             // Scan if the word starts with a vowel
@@ -30,16 +30,32 @@ public class A5 {
                     origWord.startsWith("e") || 
                     origWord.startsWith("i") || 
                     origWord.startsWith("o") ||
-                    origWord.startsWith("u")); {
+                    origWord.startsWith("u")) {
                         // add the ub
-                        origWord.startsWith("Ub");
-                        System.out.println(origWord);
-            }
-                
-        }
-        
-        
-        
-        
+                        String transWord = ("ub" + origWord);
+                        System.out.println(transWord);
+            }else{
+                // Look for a vowel in the work
+                int length = origWord.length();
+                // make a for loop to check the character
+                for (int i = 0; i < length; i++){
+                    // check for vowels
+                    if (origWord.charAt(i) == 'a' ||
+                            origWord.charAt(i) == 'e' ||
+                            origWord.charAt(i) == 'i' ||
+                            origWord.charAt(i) == 'o' ||
+                            origWord.charAt(i) == 'u'){
+                    // break the word apart at the vowel
+                        String start = origWord.substring(0, i);
+                        String end = origWord.substring(i);
+                                              
+                        String transword = (start + "ub" + end);
+                        System.out.println(transword);
+                        
+                        break;
+                }
+                }
+            }           
+        } 
     }
 }
