2c44fe1a8b5a1cf6d74d5c7c00fb4bbdc6ca6eb0
Mohamad <Mohamad@192.168.0.22>
2018-04-25 22:55:11 -0400

    big boy 123

--- src/Assignment6/A6Q3.java
+++ src/Assignment6/A6Q3.java
@@ -21,16 +21,17 @@ public class A6Q3 {
         Scanner input = new Scanner(System.in);
         // ask for the two integers
         System.out.println("Enter two integers:");
-        
+        // create the array
         int numbers[] = new int[2];
-        
+        // get the two integers
         numbers[0] = input.nextInt();
         numbers[1] = input.nextInt();
-        
+        // if the first integer is bigger than the second then switch places
         if(numbers[0] > numbers[1]){
             numbers[1] = numbers[0];
             numbers[0] = numbers[1];
         }
+        // display the result
         System.out.print("the integers in ascending order are: " + numbers[0] + " " + numbers[1]);
     }
     
--- src/Assignment6/A6Q6.java
+++ src/Assignment6/A6Q6.java
@@ -28,24 +28,26 @@ public class A6Q6 {
                  
         // create the classroom
         double[] room = new double[i];
-        
+        // ask for the marks
         System.out.println("enter the marks");
-        
+        // make for loop to collect the marks
         for(int g = 0; g < room.length; g++){            
             room[g] = input.nextDouble();
         }
-        
+        // sort the array from lowest to highest
         Arrays.sort(room);
+        // create a double for the sum
         double sum = 0;
+        // create for loop to add all marks together
         for(int g = 0; g < room.length; g++){            
             sum = sum + room[g];
         }
-        
+        // divide marks sum by the amount of marks
         sum = sum/i;
-        
-        System.out.println("The lowest mark in the class is " + room[0]);
-        System.out.println("The highest mark in the class is " + room[i]);
-        System.out.println("The class average is " + room[i]);
+        // display all marks
+        System.out.println("The lowest mark in the class is " + room[0] + "%");
+        System.out.println("The highest mark in the class is " + room[i - 1] + "%");
+        System.out.println("The class average is " + sum + "%");
    
     }
 }
