438fc3405b1f3d85c7d4644b5ccf9b93d776b945
elmam0030 <elmam0030@HRH-IC0027338.wrdsb.ca>
2018-04-26 11:18:36 -0400

    big man

--- src/Assignment6/A6Q5.java
+++ src/Assignment6/A6Q5.java
@@ -16,10 +16,52 @@ public class A6Q5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // scan
+         // Scan
         Scanner input = new Scanner(System.in);
         
+        // ask for how many marks will be recorded
+        System.out.println("How many marks will be recorded?");
+        
+        // collect the number
+        int fun = input.nextInt();
+        
+        // ask for the marks
+        System.out.println("Enter in the marks");
+                 
+        // create the array
+        double[] mark = new double[fun];
+        
+        // create for loop to get all marks
+        for(int g = 0; g < mark.length; g++){            
+            mark[g] = input.nextDouble();
+        }
+        // create a for loop
+        for(int i = 0; i < mark.length; i++){
+            
+            // create a another for loop for x+1
+            for(int g = i+1; g < mark.length; g++){
+                
+            // assign spots of arrays to doubles
+            double spotX = mark[i];
+            double spotY = mark[g];
+            
+            // if spotx is greater than spoty, switch them
+            if(spotX > spotY){
+                mark[g] = spotX;
+                mark[i] = spotY;
+                
+                }
+            }            
+        }
         
         
+        // display all the numbers
+        System.out.print("The marks in ascending order are: ");
+        
+        
+        for(int g = 0; g < mark.length; g++){ 
+            
+            System.out.print(mark[g] + " ");
+        }
     }
 }
