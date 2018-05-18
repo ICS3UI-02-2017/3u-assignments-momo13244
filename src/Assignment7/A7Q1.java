7d7d69fdfaf690e6a5db7efaa074a47ddf66df03
elmam0030 <elmam0030@HRH-IC0027338.wrdsb.ca>
2018-05-18 11:18:55 -0400

    big man

--- src/Assignment7/A7Q1.java
+++ src/Assignment7/A7Q1.java
@@ -4,13 +4,21 @@
  */
 package Assignment7;
 
+import java.util.Scanner;
+
 /**
  *
  * @author elmam0030
  */
-public class A7Q1 {
+public class A7Q1 { 
     
     public static double circleArea(String[] args){
+        
+        
+        // make the calculator for area
+        double Area = Math.pow(rad, 2);
+        
+        
         
     }
 
@@ -18,6 +26,17 @@ public class A7Q1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // scan
+        Scanner input = new Scanner(System.in);
+        
+        // ask for radius
+        System.out.println("Enter the radius: ");
+        
+        // get radius
+        double rad = input.nextDouble();
+        
+        // Output the area
+        System.out.println("The area of the circle is: " + (circleArea(numbers)));
         
     }
 }
